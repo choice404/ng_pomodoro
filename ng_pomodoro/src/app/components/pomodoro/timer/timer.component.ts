6f981e7fe299a360047966c7ba3e6af7b9b7b8f0
choisauce <austinch20@protonmail.com>
2023-12-25 23:32:28 -0800

    moved stuff around

---
+++ ng_pomodoro/src/app/components/pomodoro/timer/timer.component.ts
@@ -0,0 +1,169 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent {
+  minutes: number = 0;
+  seconds: number = 0;
+  minutesDisplay: string = "25";
+  secondsDisplay: string = "00";
+  timer: string = "pomodoro";
+  isTimerRunning: boolean = false;
+  buttonLabel: string = "Start";
+  pomodoroCount: number = 0;
+  interval: any;
+
+  selectPomodoro()
+  {
+    if(this.isTimerRunning)
+    {
+      alert("Timer is currently running!");
+      return;
+    }
+    this.timer = "pomodoro";
+    this.minutes = 25;
+    this.seconds = 0;
+    this.minutesDisplay = this.displayTimer(this.minutes);
+    this.secondsDisplay = this.displayTimer(this.seconds);
+  }
+
+  selectShortBreak()
+  {
+    if(this.isTimerRunning)
+    {
+      alert("Timer is currently running!");
+      return;
+    }
+    this.timer = "shortBreak";
+    this.minutes = 5;
+    this.seconds = 0;
+    this.minutesDisplay = this.displayTimer(this.minutes);
+    this.secondsDisplay = this.displayTimer(this.seconds);
+  }
+
+  selectLongBreak()
+  {
+    if(this.isTimerRunning)
+    {
+      alert("Timer is currently running!");
+      return;
+    }
+    this.timer = "longBreak";
+    this.minutes = 15;
+    this.seconds = 0;
+    this.minutesDisplay = this.displayTimer(this.minutes);
+    this.secondsDisplay = this.displayTimer(this.seconds);
+  }
+
+  displayTimer(time: number)
+  {
+    return time < 10 ? "0" + time: time.toString();
+  }
+
+  handleTimer()
+  {
+    if(this.buttonLabel === "Start")
+    {
+      this.startTimer();
+      this.buttonLabel = "Pause";
+    }
+    else if(this.buttonLabel === "Pause")
+    {
+      this.pauseTimer();
+      this.buttonLabel = "Start";
+    }
+  }
+
+  resetTimer()
+  {
+    if(this.isTimerRunning)
+    {
+      alert("Timer is currently running!");
+      return;
+    }
+
+    if(this.timer === "pomodoro")
+    {
+      this.minutes = 25;
+      this.seconds = 0;
+      this.minutesDisplay = this.displayTimer(this.minutes);
+      this.secondsDisplay = this.displayTimer(this.seconds);
+    }
+    else if(this.timer === "shortBreak")
+    {
+      this.minutes = 5;
+      this.seconds = 0;
+      this.minutesDisplay = this.displayTimer(this.minutes);
+      this.secondsDisplay = this.displayTimer(this.seconds);
+    }
+    else if(this.timer === "longBreak")
+    {
+      this.minutes = 15;
+      this.seconds = 0;
+      this.minutesDisplay = this.displayTimer(this.minutes);
+      this.secondsDisplay = this.displayTimer(this.seconds);
+    }
+  }
+
+  pauseTimer()
+  {
+    this.isTimerRunning = false;
+    clearInterval(this.interval);
+  }
+
+  startTimer()
+  {
+    let minutes = parseInt(this.minutesDisplay);
+    let seconds = parseInt(this.secondsDisplay);
+    let totalSeconds = minutes * 60 + seconds;
+
+    console.log("Total seconds: " + totalSeconds);
+    console.log("Minutes: " + minutes);
+    console.log("Seconds: " + seconds);
+
+    this.isTimerRunning = true;
+
+    const timer = () =>
+    {
+      totalSeconds--;
+      let minutesLeft = Math.floor(totalSeconds / 60);
+      let secondsLeft = totalSeconds % 60;
+
+      this.minutesDisplay = this.displayTimer(minutesLeft);
+      this.secondsDisplay = this.displayTimer(secondsLeft);
+      if(minutesLeft <= 0 && secondsLeft <= 0)
+      {
+        this.isTimerRunning = false;
+        clearInterval(this.interval);
+        alert("Timer is done!");
+        if(this.timer === "pomodoro")
+        {
+          this.pomodoroCount++;
+          // this.emitPomodoroo.emit()
+          if(this.pomodoroCount === 4)
+          {
+            this.pomodoroCount = 0;
+            this.selectLongBreak();
+          }
+          else
+          {
+            this.selectShortBreak();
+          }
+        }
+        else if(this.timer === "shortBreak")
+        {
+          this.selectPomodoro();
+        }
+        else if(this.timer === "longBreak")
+        {
+          this.selectPomodoro();
+          this.pomodoroCount = 0;
+        }
+      }
+    }
+    this.interval = setInterval(timer, 1000);
+  }
+}
